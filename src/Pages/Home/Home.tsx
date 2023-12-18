a423197c8180dea18a1510d7fb242390f0b16b0c
Maymonakh <maymonakhanfar@gmail.com>
2023-12-18 21:04:31 +0200

    add navbar component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Navbar from "../../Components/NavBar/Navbar"
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  return <Navbar />;
 };
 
 export default Home;
